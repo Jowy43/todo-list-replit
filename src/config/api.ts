69a4e9d2960f3aef563adcccb845f2eb74984d48
joelps117 <36492101-joelps117@users.noreply.replit.com>
2025-02-21 22:37:26 +0000

    User checkpoint: Update JSONBin API ID and URL configuration.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,5 @@
-
 export const API_CONFIG = {
-  JSONBIN_ID: "67b8da07acd3cb34a8eb8518",
+  JSONBIN_ID: "67b8e50fad19ca34f80d15b1",
   JSONBIN_KEY: "$2a$10$ca0/pCcVzNay1ZiXTTPXpefAkB20ooNsbMHuze6JXDzh3rXb1pekO",
-  JSONBIN_URL: "https://api.jsonbin.io/v3/b"
+  JSONBIN_URL: "https://api.jsonbin.io/v3/b",
 };
